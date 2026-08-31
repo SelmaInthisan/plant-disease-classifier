9a306f61bfd83bae687bcaea482cb597bcbca30d
SelmaInthisan <inthisansalma@gmail.com>
2026-08-31 19:48:44 +0530

    Fix Render port and startup

--- app_hf.py
+++ app_hf.py
@@ -290,8 +290,7 @@ with gr.Blocks(
 
 
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", "7860"))
+    port = int(os.environ.get("PORT", "10000"))
 
     demo.launch(
         server_name="0.0.0.0",
